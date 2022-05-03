229cdb1805fc1ccc8f54c6c530ffaccd35bf4fef
ManuelBenini <manuelbenini1905@gmail.com>
2022-05-03 14:12:35 +0200

    Primo commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const app = new Vue({
+
+    el: '#app',
+
+    data: {
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ]
+    }
+
+});
+
+/*
+    const slides = [
+        {
+            image: 'img/01.jpg',
+            title: 'Svezia',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        },
+        {
+            image: 'img/02.jpg',
+            title: 'Svizzera',
+            text: 'Lorem ipsum.',
+        },
+        {
+            image: 'img/03.jpg',
+            title: 'Gran Bretagna',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        },
+        {
+            image: 'img/04.jpg',
+            title: 'Germania',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+        },
+        {
+            image: 'img/05.jpg',
+            title: 'Paradise',
+            text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+        }
+    ];
+    console.log(slides);
+*/
+
